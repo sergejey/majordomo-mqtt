78176dec7f7da6684c078115f8bb965ad0f9dcb5
Alexander Abolmasov <53314888+SmoKE585@users.noreply.github.com>
2021-08-19 12:27:28 +0300

    Add files via upload

---
+++ languages/mqtt_default.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+$dictionary=array(
+
+'MQTT_CLIENT'=>'Client name',
+'MQTT_HOST'=>'Host',
+'MQTT_PORT'=>'Port',
+'MQTT_QUERY'=>'Subscription path',
+'MQTT_REQ_AUTH'=>'authorization required',
+'MQTT_USERNAME'=>'Username',
+'MQTT_PASSWORD'=>'Password',
+
+);
+
+foreach ($dictionary as $k=>$v) {
+ if (!defined('LANG_'.$k)) {
+  define('LANG_'.$k, $v);
+ }
+}
+
+?>
---
+++ languages/mqtt_ru.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+$dictionary=array(
+
+'MQTT_CLIENT'=>'Имя клиента',
+'MQTT_HOST'=>'Адрес сервера',
+'MQTT_PORT'=>'Порт сервера',
+'MQTT_QUERY'=>'Путь',
+'MQTT_REQ_AUTH'=>'Требуется авторизация',
+'MQTT_USERNAME'=>'Имя пользователя',
+'MQTT_PASSWORD'=>'Пароль',
+
+
+);
+
+foreach ($dictionary as $k=>$v) {
+ if (!defined('LANG_'.$k)) {
+  define('LANG_'.$k, $v);
+ }
+}
+
+?>
