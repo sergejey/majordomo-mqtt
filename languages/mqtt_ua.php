1a1d7da49adab19bf59dbb957a0e7e25b9e5c319
tarasfrompir <tarasfrompir@inbox.ru>
2021-08-31 18:52:57 +0300

    Update mqtt_ua.php

--- languages/mqtt_ua.php
+++ languages/mqtt_ua.php
@@ -1,11 +1,11 @@
 <?php
 $dictionary=array(
-'NEW_MQTT_CLIENT'=>'Імя клієнта',
+'NEW_MQTT_CLIENT'=>'Ім\`я клієнта',
 'NEW_MQTT_HOST'=>'Адрес сервера',
 'NEW_MQTT_PORT'=>'Порт сервера',
-'NEW_MQTT_QUERY'=>'Шлях підписаних топіків',
+'NEW_MQTT_QUERY'=>'Шлях до підписаних топіків',
 'NEW_MQTT_REQ_AUTH'=>'Потребує авторизацію',
-'NEW_MQTT_USERNAME'=>'Імя користувача',
+'NEW_MQTT_USERNAME'=>'Ім\`я користувача',
 'NEW_MQTT_PASSWORD'=>'Пароль'
 );
 
