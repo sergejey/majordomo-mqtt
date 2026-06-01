03a92805d90b77ac839d4e0bfaed344f65a5eba4
Sergei Jeihala <sergejey@gmail.com>
2026-06-01 21:49:01 +0300

    Revert "Рефакторинг MQTT: цикл, ускорение обработки и форма настроек"

--- modules/mqtt/mqtt_edit.inc.php
+++ modules/mqtt/mqtt_edit.inc.php
@@ -45,14 +45,13 @@ if ($this->mode == 'update') {
     $rec['WRITE_TYPE'] = gr('write_type', 'int');
 
     //UPDATING RECORD
-        if ($ok) {
-            if ($rec['ID']) {
-                SQLUpdate($table_name, $rec); // update
-            } else {
-                $new_rec = 1;
-                $rec['ID'] = SQLInsert($table_name, $rec); // adding new record
-            }
-            $this->invalidateLookupCache();
+    if ($ok) {
+        if ($rec['ID']) {
+            SQLUpdate($table_name, $rec); // update
+        } else {
+            $new_rec = 1;
+            $rec['ID'] = SQLInsert($table_name, $rec); // adding new record
+        }
 
         if ($rec['LINKED_OBJECT'] && $rec['LINKED_PROPERTY']) {
             addLinkedProperty($rec['LINKED_OBJECT'], $rec['LINKED_PROPERTY'], $this->name);
@@ -63,7 +62,6 @@ if ($this->mode == 'update') {
 
         if ($rec['PATH_WRITE'] != '' && $rec['PATH_WRITE'] != $rec['PATH']) {
             SQLExec("DELETE FROM mqtt WHERE PATH='" . DBSafe($rec['PATH_WRITE']) . "'");
-            $this->invalidateLookupCache();
         }
 
         $out['OK'] = 1;
